0e0af42128c54be3b9c8795f0dccdf10f8d36885
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2024-10-27 18:31:30 +0300

    Update BaseEncryption.cs

--- SecureData/Encryption/BaseEncryption.cs
+++ SecureData/Encryption/BaseEncryption.cs
@@ -3,7 +3,6 @@
     // Soyut Şifreleme Sınıfı
     public abstract class BaseEncryption : IEncryption
     {
-        public abstract byte[] key { get; } // Abstract property for key // Anahtar için soyut property
         public abstract byte[] Encrypt(string plainText); // Abstract method for encryption // Şifreleme için soyut metot
         public abstract string Decrypt(byte[] cipherText); // Abstract method for decryption // Şifre çözme için soyut metot
     }
