bfda01bb03e8e442ee08ed4cc1e47e4ddebabf78
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2024-10-27 14:25:24 +0300

    Encryption abstract and interface init
    Encryption abstract and interface init

---
+++ SecureData/Encryption/IEncryption.cs
@@ -0,0 +1,10 @@
+﻿namespace Encryption
+{
+    // Şifreleme Arayüzü
+    public interface IEncryption
+    {
+        public byte[] key { get; } // Property to get the encryption key // Şifreleme anahtarını almak için property
+        byte[] Encrypt(string plainText); // Method to encrypt plaintext // Düz metni şifrelemek için metot
+        string Decrypt(byte[] cipherText); // Method to decrypt ciphertext // Şifreli metni çözmek için metot
+    }
+}
