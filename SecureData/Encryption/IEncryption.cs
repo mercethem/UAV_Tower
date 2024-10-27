1eedc15172de451c5f810eba3018431c7db94577
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2024-10-27 18:31:54 +0300

    Update IEncryption.cs

--- SecureData/Encryption/IEncryption.cs
+++ SecureData/Encryption/IEncryption.cs
@@ -3,7 +3,6 @@
     // Şifreleme Arayüzü
     public interface IEncryption
     {
-        public byte[] key { get; } // Property to get the encryption key // Şifreleme anahtarını almak için property
         byte[] Encrypt(string plainText); // Method to encrypt plaintext // Düz metni şifrelemek için metot
         string Decrypt(byte[] cipherText); // Method to decrypt ciphertext // Şifreli metni çözmek için metot
     }
