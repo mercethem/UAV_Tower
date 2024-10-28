57a8fe508930c92debe3beb496675592aa329b51
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2024-10-28 15:21:47 +0300

    Delete Database directory

--- Database/MongoDB/IWeatherJsonParser.cs
+++
@@ -1,5 +0,0 @@
-﻿public interface IWeatherJsonParser
-{
-    // Method to parse weather data from a JSON response // JSON yanıtından hava durumu verilerini ayrıştırmak için metot
-    WeatherInfo ParseWeather(string jsonResponse, string city);
-}
--- Database/MongoDB/IWeatherService.cs
+++
@@ -1,5 +0,0 @@
-﻿public interface IWeatherService
-{
-    // Asynchronous method to get weather information for a specific city // Belirli bir şehir için hava durumu bilgisi almak için asenkron metot
-    Task<WeatherInfo> GetWeatherAsync(string city);
-}
